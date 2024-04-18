09c2c89b594cbd9f76267c0d311a2137bf1c4d4b
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-04-18 22:32:59 +0530

    add registration hooks FIXME note
    update revoke session mutation

--- client/lib/hooks/useRevokeSession.ts
+++ client/lib/hooks/useRevokeSession.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import kratos from "../kratos";
 
-// TODO: update query cache here
 export default function useRevokeSession() {
   const queryClient = useQueryClient();
 
@@ -10,25 +9,8 @@ export default function useRevokeSession() {
       const { data } = await kratos.disableMySession({ id: sessionId });
       return data;
     },
-    onSuccess: async (data, {sessionId}) => {
-    
-    queryClient.setQueryData(['/sessions'], (data) => {
-      console.log("OLD DATA: ", data);
-      if (data) {
-        const newPagesArray =
-        data.pages.map((page) =>
-          page.sessions.filter((session) => session.id !== sessionId),
-        ) ?? [];
-  
-        return {
-          ...data,
-          pages: newPagesArray,
-      };
-      }
-
-      return data;
-
-      });
-    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({queryKey: ["/sessions"]})
+    }
   });
 }
